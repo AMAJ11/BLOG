acd1b57062d1e7356b8e23e3c9ecfa2eb079d286
AMAJ11 <apo.zouher@gmail.com>
2025-01-03 19:43:11 +0300

    push

---
+++ src/views/PostView.vue
@@ -0,0 +1,27 @@
+<template>
+     <div class="posts"> 
+        <v-row class="pa-8 pt-6" style="">
+
+        </v-row>
+
+     </div>
+
+
+</template>
+
+<script>
+   export default{
+    name:"PostView"
+   }
+   </script>
+<style scoped>
+.posts{  
+   height: auto;
+   min-height: 90vh;
+background-color: beige;
+   background-size: cover;
+   background-position: center;
+
+}
+
+</style>
